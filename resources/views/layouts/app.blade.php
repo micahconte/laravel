334e5482e8fa4a6f51e0908842f3f56ef1d6dea8
Micah Conte <micahconte@hotmail.com>
2016-05-25 15:27:33 -0700

    removed jQuery mobile call

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -86,7 +86,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-    <!-- <script type="text/javascript" src="{{ url('/js/jquery.mobile-1.4.5.min.js') }}"></script> -->
     <script type="text/javascript" src="{{ url('/js/fastclick.js') }}"></script>
     <script type="text/javascript" src="{{ url('/js/resume.js') }}"></script>
     <script type="text/javascript" src="{{ url('/js/datatables.js') }}"></script>
