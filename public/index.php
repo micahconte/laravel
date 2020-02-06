b00cdaa20104dd56c6d5a9872d18cd6bed452ba3
micah conte <mconte@mdsol.com>
2020-02-05 21:26:44 -0500

    Added bash command to execute config changes for amezmo

--- public/index.php
+++ public/index.php
@@ -47,6 +47,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |
 */
 
+bash_exec(".amezmo/config/after-change");
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
